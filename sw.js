a86b7ed890d48ce23d10e493adf77246eeec6b31
jisdcm29-sketch <jisdcm29@gmail.com>
2026-09-26 02:10:45 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='kwb-offline-v1.0.3';
+const CACHE_NAME='kwb-offline-v1.0.4';
 const CORE_ASSETS=[
   './','./index.html','./arena.html','./join.html',
   './word-battle.html','./play.html','./matching-pairs.html','./matching-play.html',
